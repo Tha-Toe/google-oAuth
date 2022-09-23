abe410867c2f18bd64bdcb648b5897764b175c70
tha toe saung <thatoe510@gmail.com>
2022-09-23 08:13:09 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
-import SignIn from "./pages/SignIn";
-import { AuthContextProvider } from "./context/AuthContext";
-import AccountPage from "./pages/AccountPage";
-import Protected from "./components/Protected";
 
-function App() {
+import jwt_decode from "jwt-decode";
+function App({}) {
+  const [user, setUser] = useState({});
+  function handleCallbackResponsive(responsive) {
+    console.log("Encoded JWT ID token" + responsive.credential);
+    var userObject = jwt_decode(responsive.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  const handleSignOut = (e) => {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  };
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        "555618407648-lkittruvsnt5jr327s088990pgv3bi9t.apps.googleusercontent.com",
+      callback: handleCallbackResponsive,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+
+    google.accounts.id.prompt();
+  }, []);
   return (
-    <div>
-      <AuthContextProvider>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route
-            path="/account"
-            element={
-              <Protected>
-                {" "}
-                <AccountPage />
-              </Protected>
-            }
-          />
-        </Routes>
-      </AuthContextProvider>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <h1>Google Auth</h1>
+      <div id="signInDiv"></div>
+      {Object.keys(user).length != 0 && (
+        <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+      )}
+      {user && (
+        <>
+          <img src={user.picture} />
+          <h1>{user.name}</h1>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/Home.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import GoogleButton from "react-google-button";
-export default function Home() {
-  return <h3>Please Sign In</h3>;
-}
